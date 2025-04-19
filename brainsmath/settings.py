4ea41b365a1bfff2e25c46ff954d637a18f209de
Omar Emad <om3x4e@gmail.com>
2025-04-19 23:39:11 +0200

    Update settings.py

--- brainsmath/settings.py
+++ brainsmath/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dotenv
-from datetime import deltatime
+from datetime import timedelta
 dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
